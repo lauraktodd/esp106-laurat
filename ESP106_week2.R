c4e72afca846ef4d5dbb414988131b28145a5190
Laura <145950148+lauraktodd@users.noreply.github.com>
2026-01-10 20:28:34 -0800

    Add files via upload

---
+++ ESP106_week2.R
@@ -0,0 +1,32 @@
+## This is the lab for week 2 
+## Add working R code in between the questions
+## The functions refer to the sections in https://rspatial.org/intr/
+
+## Functions
+
+## 1) Write a function 'f' that behaves like this:
+## > f('Jim')
+## [1] "hello Jim, how are you?"
+##HINT: you will need to use the paste() function within your new function
+
+
+## 2) Write a function 'sumOfSquares' that behaves like this:
+
+
+## > d <- c(1,5,2,4,6,2,4,5)
+## > sumOfSquares(d)
+## [1] 21.875
+
+# To compute the "sum of squares", subtract the mean value of all numbers from each number. 
+# Square these numbers and sum them
+# (bonus: make a variant that can handle NAs)
+
+
+## Flow Control
+
+## 4) Write a for loop that adds the numbers 1 to 10
+##HINT: you will need one variable that loops through the numbers 1 to 10 and one that keeps count through each loop
+
+
+## 5) Write a for loop that adds the odd numbers between 1 and 10 
+
